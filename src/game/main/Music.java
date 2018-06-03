657c119d45b82aa53abc307420193aaada7662ed
QinCalvin <calvin.qin.ca@gmail.com>
2018-06-02 18:27:30 -0700

    Started music

---
+++ src/game/main/Music.java
@@ -0,0 +1,9 @@
+package game.main;
+
+public class Music {
+    private final static String[] songs = new String[2];
+
+    public static void initSongs() {
+        songs[0] = "one song here";
+    }
+}
