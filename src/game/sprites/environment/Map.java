2e2b839e0b8e3c0d6f2c2b7419f88879d9b13baf
QinCalvin <calvin.qin.ca@gmail.com>
2018-06-05 21:56:00 -0700

    Started Map

---
+++ src/game/sprites/environment/Map.java
@@ -0,0 +1,12 @@
+package game.sprites.environment;
+
+import game.sprites.entity.Player;
+
+public class Map {
+
+    private Player player;
+
+    public Map(Player p) {
+        player = p;
+    }
+}
