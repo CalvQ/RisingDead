26f3806045da53077ede8a030037e24a75a78231
QinCalvin <calvin.qin.ca@gmail.com>
2018-06-09 07:35:53 -0700

    Added constructor

--- src/game/sprites/entity/Player.java
+++ src/game/sprites/entity/Player.java
@@ -1,11 +1,20 @@
 package game.sprites.entity;
 
+import game.sprites.Location;
+import game.sprites.environment.Map;
+
 import java.awt.*;
 
 public class Player extends Entity {
 
-    public Player(int health) {
-        this.health = health;
+    private String name;
+    private Location loc;
+
+    public Player(String n) {
+        this.health = 100;
+        this.angle = -1.57;
+        this.loc = new Location(Map.WIDTH/2,Map.HEIGHT/2);
+        name = n;
     }
 
     public void render(Graphics g) {
