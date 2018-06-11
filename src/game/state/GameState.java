93433d6f6425afa0b5b778eb8dd423e66993b211
QinCalvin <calvin.qin.ca@gmail.com>
2018-06-10 21:04:11 -0700

    Player movement and rendering

--- src/game/state/GameState.java
+++ src/game/state/GameState.java
@@ -11,7 +11,6 @@ import java.awt.event.MouseEvent;
 public class GameState extends State {
 
     private Player player;
-    private Image bodyP, bodyE;
     private boolean left, up, right, down;
     private Image[] guns = new Image[0];//Get number of guns?
     private Map m;
@@ -30,10 +29,25 @@ public class GameState extends State {
     }
 
     public void render(Graphics g) {
-
+        player.render(g, player.getLoc());
+        m.render(g, player.getLoc());
     }
 
     public void tick() {
+        player.tick();
+
+        if(left) {
+            player.moveX(-5);
+        }
+        if(right) {
+            player.moveX(5);
+        }
+        if(up) {
+            player.moveY(-5);
+        }
+        if(down) {
+            player.moveY(5);
+        }
 
         if(Music.isNotRunning()) {
             Music.playMusic();
@@ -63,7 +77,21 @@ public class GameState extends State {
     }
 
     public void processKeyEventRelease(KeyEvent ke) {
-
+        int code = ke.getKeyCode();
+        switch (code){
+            case 65:
+                left = false;
+                break;
+            case 87:
+                up = false;
+                break;
+            case 68:
+                right = false;
+                break;
+            case 83:
+                down = false;
+                break;
+        }
     }
 
     public void processMouseHold(MouseEvent e) {
